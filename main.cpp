86b292aac29b6226d4f9a0cd92e43f88f2e754c7
Matteo Bir <bir.matteo@gmail.com>
2025-01-11 22:24:55 -0800

    bare window

--- main.cpp
+++ main.cpp
@@ -6,41 +6,52 @@
 #include <cstdlib>
 
 class HelloTriangleApplication {
-public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
+ public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+ private:
+  const int WIDTH = 800;
+  const int HEIGHT = 600;
+  GLFWwindow* window;
+
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+
+  void initVulkan() {
+
+  }
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
     }
+  }
 
-private:
-    void initWindow() {
+  void cleanup() {
+    glfwDestroyWindow(window);
 
-    }
-
-    void initVulkan() {
-
-    }
-
-    void mainLoop() {
-
-    }
-
-    void cleanup() {
-
-    }
+    glfwTerminate();
+  }
 };
 
 int main() {
-    HelloTriangleApplication app;
+  HelloTriangleApplication app;
 
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+  try {
+    app.run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
